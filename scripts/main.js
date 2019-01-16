979331a4e84ddd09ec84a0d5685a2a602bec8d99
Sreejith Sreekumar <sreejith2904@gmail.com>
2019-01-16 12:45:33 -0500

    delete unwanted images

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,9 @@
-
-
 $(document).ready(function () {
      var person = prompt("Please enter your name to continue");
      if (person != null) {
 	  document.getElementById("p1").innerHTML = person;
       }
 });
-
-
 
 
 function clickImg1() {
