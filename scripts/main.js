45e0ea91cb75c98b8138028a21e29d31dac51670
Sreejith <sreejith2904@gmail.com>
2019-01-16 02:00:57 -0500

    add main code, css, js and index

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+
+function updateAlexaCount(){
+    alert("Voted for Alexa!");
+}
+
+
+function updateHomeCount(){
+    alert("Voted for Google Home!");    
+}
+
+function updateSiriCount(){
+    alert("Voted for Siri");
+}
