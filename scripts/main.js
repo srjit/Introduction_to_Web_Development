e52b35010946b2412d8a0a61025da50a57b49bb1
Sreejith <sreejith2904@gmail.com>
2019-01-16 02:25:44 -0500

    update js files

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,27 @@
 
 
 function updateAlexaCount(){
-    alert("Voted for Alexa!");
+    
+    var currentCount = document.getElementById("alexa_vote_count").value();
+    currentCount += 1;
+    document.getElementById("alexa_vote_count").value = currentCount;
+    alert("Current Alexa count: !" + currentCount);
 }
 
 
 function updateHomeCount(){
-    alert("Voted for Google Home!");    
+    alert("Voted for Google Home!");
+    var currentCount = document.getElementById("home_vote_count").value();
+    currentCount += 1;
+    document.getElementById("home_vote_count").value = currentCount;
+    alert("Current Google Home vote count: !" + currentCount);    
 }
 
 function updateSiriCount(){
     alert("Voted for Siri");
+    var currentCount = document.getElementById("siri_vote_count").value()
+    currentCount += 1;
+    document.getElementById("siri_vote_count").value = currentCount;
+    alert("Current Siri vote count: !" + currentCount);    
+    
 }
